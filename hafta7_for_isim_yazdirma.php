872df529c587a42cd6ac6e6d74dcfb1e1334d6c8
efeeyucc <muhammedefeyuce76@gmail.com>
2025-01-03 17:41:33 +0300

    Add files via upload

---
+++ hafta7_for_isim_yazdirma.php
@@ -0,0 +1,6 @@
+<?php
+for ($i=1;$i<=7;$i++)
+{
+    echo "<font size=".$i."<b>Muhammed Efe Yüce</b>"."<br>"."</font>";
+}
+?>
---
+++ hafta9_cok_boyutlu_dizi.php
@@ -0,0 +1,9 @@
+<?php
+$ogrenci=array();
+$ogrenci[0]=array("Mert","Balıkesir");
+$ogrenci[1]=array("Muhammed","Iğdır");
+$ogrenci[2]=array("Nilay","Sinop");
+echo "<pre>";
+print_r($ogrenci);
+echo "</pre>";
+?>
